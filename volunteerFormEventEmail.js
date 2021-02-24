5474913a97a43f33928dd30a0324736e44e27814
Brad Boggs <bboggs@bboggs-mbp.local>
2021-02-24 09:08:36 -0600

    update code to work with real form and spread sheet

--- volunteerFormEventEmail.js
+++ volunteerFormEventEmail.js
@@ -1,6 +1,22 @@
-const SPREADSHEET_ID = ""
-const SPREADSHEET_NAME = ""
-const EMAIL_ADDRESSES = "bradleywboggs@gmail.com"
+const SPREADSHEET_ID = ''
+
+// Cafe Locations
+const WESTRIDGE_LOCATION = 'West Ridge Mall' 
+const SEVENTEENTH_ST_LOCATION = '17th St' 
+const KCMO_LOCATION = 'KCMO' 
+
+// The DEFAULT_EMAIL_ADDRESSES are for testing or
+// if you want to have one or more people always get the email in addition to the location specific email address
+// If you don't want one person to get them all, simply set the value to an empty string: ''
+const DEFAULT_EMAIL_ADDRESSES = ''
+
+const EMAILS_BY_LOCATION = {
+  [WESTRIDGE_LOCATION]: 'MadisenW@CafeBarnabas.org',
+  [SEVENTEENTH_ST_LOCATION]:'ChristenB@CafeBarnabas.org',
+  [KCMO_LOCATION]: 'LookingF@CafeBarnabas.org'
+}
+
+const EMAIL_SUBJECT = 'New Volunteer Application!'
 const EMAIL_TEMPLATE = `
 <!DOCTYPE html>
 <html>
@@ -16,61 +32,103 @@ const EMAIL_TEMPLATE = `
  }
   </style>
 </head>  
-<h1>Hey World Changer!!!</h1>
-<h2>We have a new volunteer application. Thanks for seizing this relational opportunity in the next 24-48 hours!</h2>
-<h3>CREATING OUTSTANDING EXPERIENCES</h3>
-<p><strong>Rom. 10:14-15</strong><br/>
-<em>How then will they call on him in whom they have not believed?<br/>
-And how are they to believe in him of whom they have never heard? <br/>
-And how are they to hear without someone preaching? <br/>
-And how are they to preach unless they are sent?<br/>
-As it is written, “How beautiful are the feet of those who preach the good news!”
-</em>
-</p>
-
-<h2>Volunteer Info</h2>
-<table>
-{{VOLUNTEER_DATA}}
-</table>
-<p>In Christ's Service,<br/> 
-The Cafe Barnabas Team</p>
-
-
-<footer>
-Topeka:West Ridge Mall | <a href=mailto:Madisen@CafeBarnabas.org>Madisen@CafeBarnabas.org</a> <br/>
-Topeka:17th St | <a href=mailto:ChristenB@CafeBarnabas.org>ChristenB@CafeBarnabas.org</a>  <br/>
-KCMO:Truman Ave | <a href=mailto:LookingF@CafeBarnabas.org>LookingF@CafeBarnabas.org</a>  <br/>
-</footer>
+<body>
+  <h1>Hey World Changer!!!</h1>
+  <h2>We have a new volunteer application. Thanks for seizing this relational opportunity in the next 24-48 hours!</h2>
+  <h3>CREATING OUTSTANDING EXPERIENCES</h3>
+  <p>
+    <strong>Rom. 10:14-15</strong><br/>
+    <em>
+      How then will they call on him in whom they have not believed?<br/>
+      And how are they to believe in him of whom they have never heard? <br/>
+      And how are they to hear without someone preaching? <br/>
+      And how are they to preach unless they are sent?<br/>
+      As it is written, “How beautiful are the feet of those who preach the good news!”
+    </em>
+  </p>
+  <h2>Volunteer Info</h2>
+  <table>
+  {{VOLUNTEER_DATA}}
+  </table>
+  <p>
+    In Christ's Service,<br/> 
+    The Cafe Barnabas Team
+  </p>
+</body> 
 </html>
 `
-
-
-/* 
-    FIELDS consists of an object whose keys are the field names for the email,
-    whose values are the column numbers for the corresponding cells on the spread sheet
- */
 const FIELDS = {
-  "Name": 1,
-  "School": 2, 
-  "Phone": 3,
-  "Why They Want To Volunteer": 4,
+  Name: {
+    isMultiColumn: true,
+    columnNumber: [1,2],
+    separator: ' ',
+    },
+  School: {
+    isMultiColumn: false,
+    columnNumber: 37
+    },
+   Email: {
+     isMultiColumn: false,
+     columnNumber: 33
+   },  
+  Phone: {
+    isMultiColumn: false,
+    columnNumber: 7
+    },
+  'Why They Want To Volunteer':{
+    isMultiColumn: false,
+    columnNumber: 36
+    },
+  Location: {
+    isMultiColumn: false,
+    columnNumber: 34
+  }
 }
 
-
 const fetchDataFromLastRow = () => {
-  const sheet = SpreadsheetApp.openById(SPREADSHEET_ID).getSheetByName(SPREADSHEET_NAME)
-  return sheet.getDataRange().getValues()[sheet.getLastRow() - 1]
+  const ss = SpreadsheetApp.openById(SPREADSHEET_ID)
+  const sheet = ss.getActiveSheet()
+  const data = sheet.getDataRange().getValues()
+  return data[sheet.getLastRow() - 1]
 }
 
 const createLastRowDataObject = (data, fields) => {
-    return Object.entries(fields).reduce((accum, [field, idx]) => {
-      accum[field] = data[idx]
+  return Object.entries(fields).reduce((accum, [field, value]) => {
+    if (value.isMultiColumn) {
+      accum[field] = value.columnNumber.reduce((_accum, colNum) => {
+        const prefix = _accum == '' ? _accum : `${_accum}${value.separator}`
+        return `${prefix}${data[colNum]}`
+      }, '')
       return accum
-    }, {})
+    } else {
+      accum[field] = data[value.columnNumber]
+      return accum
+    }
+  }, {})
+}
+
+const getEmailAddressByLocation = (location) => {
+    if (location.includes(SEVENTEENTH_ST_LOCATION)) {
+      return EMAILS_BY_LOCATION[SEVENTEENTH_ST_LOCATION]
+    }
+    if (location.includes(KCMO_LOCATION)) {
+      return EMAILS_BY_LOCATION[KCMO_LOCATION]
+    }
+    return EMAILS_BY_LOCATION[WESTRIDGE_LOCATION]
+}
+
+const aggregateEmailAddresses = (emailByLocation, defaultEmails) => {
+  if (defaultEmails === '') {
+    return emailByLocation
+  }
+  return `${defaultEmails}, ${emailByLocation}`
 }
 
 const prepareHtml = (data, template) => {
   const volunteerData = Object.entries(data).reduce((accum, [key, val]) => {
+    if (key === 'Location') {
+      return accum
+    } else {
     return `
     ${accum}
 	    <tr>
@@ -78,23 +136,27 @@ const prepareHtml = (data, template) => {
        	      <td>${val}</td>
       </tr>
       `
-  }, "")
+  }}, '')
   return `${template.replace('{{VOLUNTEER_DATA}}', volunteerData)}`
 }
 
-const onUpdate = () => {
+const onSubmit = () => {
   // 1. grab the data from the last row
   const dataFromLastRow = fetchDataFromLastRow()
-  // 2. aggregate only the needed fields as an object
+  // 2. aggregate needed fields and values as an object
   const processedData = createLastRowDataObject(dataFromLastRow, FIELDS)
-  // 3. Create HTML payload with processedData
+  // 3. get the location specific email addressee
+  const emailByLocation = getEmailAddressByLocation(processedData.Location)
+  // 4. aggregate the above email with any existing default email addressees
+  const allEmailAddresses = aggregateEmailAddresses(emailByLocation, DEFAULT_EMAIL_ADDRESSES)
+  // 5. Create HTML payload with processedData
   const emailBody = prepareHtml(processedData, EMAIL_TEMPLATE)
-  // 4. Send emails
+  // 6. Send emails
   MailApp.sendEmail(
     {
-      "to": EMAIL_ADDRESSES,
-      "htmlBody": emailBody,
-      "subject": "New Volunteer Application!"
+      'to': allEmailAddresses,
+      'htmlBody': emailBody,
+      'subject': EMAIL_SUBJECT
       }
     )
 } 
